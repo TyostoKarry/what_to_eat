4c31d9db6da8b3502ff018a81720d0bfec7fbf2a
TyostoKarry <samuritala@hotmail.com>
2024-09-20 15:00:31 +0300

    Fix categories grid not working on small screens

--- lib/components/what_to_eat_categories.dart
+++ lib/components/what_to_eat_categories.dart
@@ -14,7 +14,6 @@ class WhatToEatCategories extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.end, // Push the grid to the bottom
         children: [
           Container(
             padding: const EdgeInsets.symmetric(vertical: 20),
@@ -29,46 +28,43 @@ class WhatToEatCategories extends StatelessWidget {
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: GridView.builder(
-              shrinkWrap:
-                  true, // Ensures the grid takes up the necessary space and no more
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, // 2 items per row (2x3 grid)
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                childAspectRatio: 1, // square-shaped items
-              ),
-              itemCount: categories.length,
-              itemBuilder: (context, index) {
-                return Material(
-                  color: Colors.blue[200],
-                  elevation: 4,
-                  borderRadius: BorderRadius.circular(16),
-                  child: InkWell(
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, // 2 items per row
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 1, // square-shaped items
+                ),
+                itemCount: categories.length,
+                itemBuilder: (context, index) {
+                  return Material(
+                    color: Colors.blue[200],
+                    elevation: 4,
                     borderRadius: BorderRadius.circular(16),
-                    splashColor: Colors.blue[800],
-                    onTap: () {
-                      // Handle the onTap event here
-                    },
-                    child: Center(
-                      child: Text(
-                        categories[index], // Use the category from the list
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(16),
+                      splashColor: Colors.blue[800],
+                      onTap: () {
+                        // Handle the onTap event here
+                      },
+                      child: Center(
+                        child: Text(
+                          categories[index], // Use the category from the list
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
-          ),
-          SizedBox(
-            height: 50,
           ),
         ],
       ),
