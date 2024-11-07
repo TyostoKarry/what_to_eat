68f392f7fef882773a9f490fe2da159cafb2d213
TyostoKarry <114697841+TyostoKarry@users.noreply.github.com>
2024-11-07 14:50:53 +0200

    Refactor entry_screen components into their own widgets

--- lib/core/screens/entry_screen.dart
+++ lib/core/screens/entry_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:what_to_eat/shared/theme/app_colors.dart';
 import 'package:what_to_eat/shared/widgets/wte_safe_area.dart';
 import 'package:what_to_eat/core/widgets/wte_split_screen_animation.dart';
-import 'package:what_to_eat/shared/widgets/wte_text.dart';
 
 class EntryScreen extends StatefulWidget {
   final Function onItemTapped;
@@ -38,173 +37,235 @@ class EntryScreenState extends State<EntryScreen> {
         child: Row(
           children: <Widget>[
             Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: AppColors.getWhatToEatBackground(),
-                ),
-                child: InkWell(
-                  onTap: () {
-                    _onSideSelected(1);
-                  },
-                  splashColor: AppColors.splashColor,
-                  child: const Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          Icons.restaurant_sharp,
-                          size: 36,
-                          color: AppColors.textPrimaryColor,
-                          shadows: <Shadow>[
-                            Shadow(
-                              offset: Offset(2, 2),
-                              blurRadius: 3,
-                              color: Color.fromARGB(140, 110, 110, 110),
-                            ),
-                          ],
-                        ),
-                        Text(
-                          'What',
-                          style: TextStyle(
-                            fontSize: 36,
-                            color: AppColors.textPrimaryColor,
-                            shadows: <Shadow>[
-                              Shadow(
-                                offset: Offset(2, 2),
-                                blurRadius: 3,
-                                color: Color.fromARGB(140, 110, 110, 110),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Text(
-                          'to Eat',
-                          style: TextStyle(
-                            fontSize: 36,
-                            color: AppColors.textPrimaryColor,
-                            shadows: <Shadow>[
-                              Shadow(
-                                offset: Offset(2, 2),
-                                blurRadius: 3,
-                                color: Color.fromARGB(140, 110, 110, 110),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+              child: WhatToEatWidget(
+                onTap: () {
+                  _onSideSelected(1);
+                },
               ),
             ),
             Expanded(
               child: Stack(
                 children: <Widget>[
-                  Container(
-                    decoration: BoxDecoration(
-                      gradient: AppColors.getWhereToEatBackground(),
-                    ),
-                    child: InkWell(
-                      onTap: () {
-                        _onSideSelected(2);
-                      },
-                      splashColor: AppColors.splashColor,
-                      child: const Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Icon(
-                              Icons.location_on,
-                              size: 36,
-                              color: AppColors.textPrimaryColor,
-                              shadows: <Shadow>[
-                                Shadow(
-                                  offset: Offset(2, 2),
-                                  blurRadius: 3,
-                                  color: Color.fromARGB(140, 110, 110, 110),
-                                ),
-                              ],
-                            ),
-                            Text(
-                              'Where',
-                              style: TextStyle(
-                                fontSize: 36,
-                                color: AppColors.textPrimaryColor,
-                                shadows: <Shadow>[
-                                  Shadow(
-                                    offset: Offset(2, 2),
-                                    blurRadius: 3,
-                                    color: Color.fromARGB(140, 110, 110, 110),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Text(
-                              'to Eat',
-                              style: TextStyle(
-                                fontSize: 36,
-                                color: AppColors.textPrimaryColor,
-                                shadows: <Shadow>[
-                                  Shadow(
-                                    offset: Offset(2, 2),
-                                    blurRadius: 3,
-                                    color: Color.fromARGB(140, 110, 110, 110),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+                  WhereToEatWidget(
+                    onTap: () {
+                      _onSideSelected(2);
+                    },
                   ),
-                  Positioned(
-                    bottom: 40,
-                    left: 0,
-                    right: 0,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 8,
-                        vertical: 8,
-                      ),
-                      child: SizedBox(
-                        height: 60,
-                        width: double.infinity,
-                        child: Material(
-                          color: Colors.transparent,
-                          elevation: 4,
-                          borderRadius: BorderRadius.circular(12),
-                          child: Ink(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12),
-                              gradient: AppColors.getWhoToPayBackground(),
-                            ),
-                            child: InkWell(
-                              onTap: () {
-                                _onSideSelected(3);
-                              },
-                              child: const Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: <Widget>[
-                                  Icon(Icons.paid),
-                                  WTEText(
-                                    text: "Who To Pay",
-                                    color: AppColors.textPrimaryColor,
-                                    fontSize: 24,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
+                  WhoToPayWidget(
+                    onTap: () {
+                      _onSideSelected(3);
+                    },
                   ),
                 ],
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class WhatToEatWidget extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const WhatToEatWidget({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: AppColors.getWhatToEatBackground(),
+      ),
+      child: InkWell(
+        onTap: onTap,
+        splashColor: AppColors.splashColor,
+        child: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.restaurant_sharp,
+                size: 36,
+                color: AppColors.textPrimaryColor,
+                shadows: <Shadow>[
+                  Shadow(
+                    offset: Offset(2, 2),
+                    blurRadius: 3,
+                    color: Color.fromARGB(140, 110, 110, 110),
+                  ),
+                ],
+              ),
+              Text(
+                'What',
+                style: TextStyle(
+                  fontSize: 36,
+                  color: AppColors.textPrimaryColor,
+                  shadows: <Shadow>[
+                    Shadow(
+                      offset: Offset(2, 2),
+                      blurRadius: 3,
+                      color: Color.fromARGB(140, 110, 110, 110),
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                'to Eat',
+                style: TextStyle(
+                  fontSize: 36,
+                  color: AppColors.textPrimaryColor,
+                  shadows: <Shadow>[
+                    Shadow(
+                      offset: Offset(2, 2),
+                      blurRadius: 3,
+                      color: Color.fromARGB(140, 110, 110, 110),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WhereToEatWidget extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const WhereToEatWidget({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: AppColors.getWhereToEatBackground(),
+      ),
+      child: InkWell(
+        onTap: onTap,
+        splashColor: AppColors.splashColor,
+        child: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.location_on,
+                size: 36,
+                color: AppColors.textPrimaryColor,
+                shadows: <Shadow>[
+                  Shadow(
+                    offset: Offset(2, 2),
+                    blurRadius: 3,
+                    color: Color.fromARGB(140, 110, 110, 110),
+                  ),
+                ],
+              ),
+              Text(
+                'Where',
+                style: TextStyle(
+                  fontSize: 36,
+                  color: AppColors.textPrimaryColor,
+                  shadows: <Shadow>[
+                    Shadow(
+                      offset: Offset(2, 2),
+                      blurRadius: 3,
+                      color: Color.fromARGB(140, 110, 110, 110),
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                'to Eat',
+                style: TextStyle(
+                  fontSize: 36,
+                  color: AppColors.textPrimaryColor,
+                  shadows: <Shadow>[
+                    Shadow(
+                      offset: Offset(2, 2),
+                      blurRadius: 3,
+                      color: Color.fromARGB(140, 110, 110, 110),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WhoToPayWidget extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const WhoToPayWidget({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 40,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 8,
+          vertical: 8,
+        ),
+        child: SizedBox(
+          height: 50,
+          width: double.infinity,
+          child: Material(
+            color: Colors.transparent,
+            elevation: 4,
+            borderRadius: BorderRadius.circular(12),
+            child: Ink(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                gradient: AppColors.getWhoToPayBackground(),
+              ),
+              child: InkWell(
+                onTap: onTap,
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(
+                      Icons.paid,
+                      color: AppColors.textPrimaryColor,
+                      shadows: <Shadow>[
+                        Shadow(
+                          offset: Offset(2, 2),
+                          blurRadius: 3,
+                          color: Color.fromARGB(140, 110, 110, 110),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'Who To Pay',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: AppColors.textPrimaryColor,
+                        shadows: <Shadow>[
+                          Shadow(
+                            offset: Offset(2, 2),
+                            blurRadius: 3,
+                            color: Color.fromARGB(
+                              140,
+                              110,
+                              110,
+                              110,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
